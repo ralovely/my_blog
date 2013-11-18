3445138ed8448c37753175e122da88e570bafc8f
Raphael Campardou <ralovely@gmail.com>
2013-11-18 15:29:22 +1100

    initial crappy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyBlog::Application.config.secret_key_base = '9fdae8749bcbb8b6a3def00b7e9ddc50b326ac9a3ba4d10b3fb270a5d04700d331e6dcd3dd4cf6b7f843b5d430e5d5d4c273e4c5693c22a7061d22559929a29d'
